46385b791c7b0fcb3f330658c122d7e7f496e160
Tekena Solomon <tekena157@gmail.com>
2020-07-15 18:43:02 +0100

    added dsa service

---
+++ src/pages/admin/services/dsa.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const baseUrl = '/api/dsa'
+
+const getAll = async () => {
+  const dsas = await axios.get(baseUrl)
+  return dsas.data
+}
+
+const remove = async (id) => {
+  const response = await axios.delete(`${baseUrl}/${id}`)
+  return response
+}
+
+const create = async (data) => {
+  const response = await axios.post(baseUrl, data)
+  return response.data
+}
+
+export default { getAll, remove, create }
